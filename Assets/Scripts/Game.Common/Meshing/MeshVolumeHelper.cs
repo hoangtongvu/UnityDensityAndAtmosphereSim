28f233079037dbc670656d000dc66192b2cf7082
hoangtongvu <hoangtongvu@gmail.com>
2026-04-10 21:33:51 +0700

    Add MeshVolumeHelper

---
+++ Assets/Scripts/Game.Common/Meshing/MeshVolumeHelper.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Game.Common.Meshing;
+
+public static class MeshVolumeHelper
+{
+    public static float VolumeOfMesh(Mesh mesh)
+    {
+        float volume = 0f;
+        var vertices = mesh.vertices;
+        var triangles = mesh.triangles;
+
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            Vector3 p1 = vertices[triangles[i]];
+            Vector3 p2 = vertices[triangles[i + 1]];
+            Vector3 p3 = vertices[triangles[i + 2]];
+
+            volume += SignedVolumeOfTriangle(p1, p2, p3);
+        }
+
+        return Mathf.Abs(volume);
+    }
+
+    private static float SignedVolumeOfTriangle(Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        return Vector3.Dot(p1, Vector3.Cross(p2, p3)) / 6f;
+    }
+}
