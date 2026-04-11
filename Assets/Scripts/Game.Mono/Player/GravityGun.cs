030a1fb5b238491fc78675d31485435c52c7650a
hoangtongvu <hoangtongvu@gmail.com>
2026-04-11 21:31:01 +0700

    Add GravityGun + add it to Player

---
+++ Assets/Scripts/Game.Mono/Player/GravityGun.cs
@@ -0,0 +1,133 @@
+using SaintsField;
+using UnityEngine;
+
+namespace Game.Mono.Player
+{
+    public class GravityGun : MonoBehaviour
+    {
+        [Header("References")]
+        public Camera cam;
+        [PositionHandle, DrawLabel] public Vector3 laserStartPosOffset;
+        public LineRenderer laser;
+
+        [Header("Settings")]
+        public float grabDistance = 30f;
+        public float moveForce = 150f;
+        public float maxVelocity = 10f;
+
+        public float holdDistance = 5f;
+        public float minHoldDistance = 2f;
+        public float maxHoldDistance = 15f;
+
+        public float shootForce = 30f;
+
+        private Rigidbody grabbedRb;
+
+        void Update()
+        {
+            HandleHoldDistance();
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                TryGrab();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                Release();
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Shoot();
+            }
+        }
+
+        void FixedUpdate()
+        {
+            UpdateLaser();
+
+            if (grabbedRb != null)
+            {
+                MoveObject();
+            }
+        }
+
+        void HandleHoldDistance()
+        {
+            if (grabbedRb)
+            {
+                holdDistance += Input.mouseScrollDelta.y;
+                holdDistance = Mathf.Clamp(holdDistance, minHoldDistance, maxHoldDistance);
+            }
+        }
+
+        void UpdateLaser()
+        {
+            if (this.grabbedRb)
+            {
+                laser.SetPosition(0, transform.position + laserStartPosOffset);
+                laser.SetPosition(1, grabbedRb.transform.position);
+            }
+            else
+            {
+                laser.SetPosition(0, Vector3.zero);
+                laser.SetPosition(1, Vector3.zero);
+            }
+        }
+
+        void TryGrab()
+        {
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, grabDistance))
+            {
+                Rigidbody rb = hit.rigidbody;
+
+                if (rb != null)
+                {
+                    grabbedRb = rb;
+                    grabbedRb.useGravity = false;
+                    grabbedRb.linearDamping = 10f; // smooth
+                }
+            }
+        }
+
+        void MoveObject()
+        {
+            Vector3 targetPos = cam.transform.position + cam.transform.forward * holdDistance;
+            Vector3 dir = targetPos - grabbedRb.position;
+
+            Vector3 force = dir * moveForce;
+            grabbedRb.AddForce(force, ForceMode.Acceleration);
+
+            // Clamp velocity
+            if (grabbedRb.linearVelocity.magnitude > maxVelocity)
+            {
+                grabbedRb.linearVelocity = grabbedRb.linearVelocity.normalized * maxVelocity;
+            }
+        }
+
+        void Release()
+        {
+            if (grabbedRb != null)
+            {
+                grabbedRb.useGravity = true;
+                grabbedRb.linearDamping = 0f;
+                grabbedRb = null;
+            }
+        }
+
+        void Shoot()
+        {
+            if (grabbedRb != null)
+            {
+                grabbedRb.useGravity = true;
+                grabbedRb.linearDamping = 0f;
+                grabbedRb.AddForce(cam.transform.forward * shootForce, ForceMode.Impulse);
+                grabbedRb = null;
+            }
+        }
+    }
+}
---
+++ Assets/Shaders/Laser.shader
@@ -0,0 +1,64 @@
+﻿Shader "Custom/LaserBeam"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (0,1,2,1)
+        _Intensity ("Intensity", Float) = 5
+        _Softness ("Edge Softness", Float) = 3
+    }
+
+    SubShader
+    {
+        Tags { "RenderPipeline"="HDRenderPipeline" "Queue"="Transparent" }
+
+        Pass
+        {
+            Name "Forward"
+            Blend One One
+            ZWrite Off
+            Cull Off
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+            #include "Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariables.hlsl"
+
+            struct Attributes
+            {
+                float3 positionOS : POSITION;
+                float2 uv         : TEXCOORD0;
+            };
+
+            struct Varyings
+            {
+                float4 positionCS : SV_POSITION;
+                float2 uv         : TEXCOORD0;
+            };
+
+            float4 _Color;
+            float _Intensity;
+            float _Softness;
+
+            Varyings vert (Attributes v)
+            {
+                Varyings o;
+                o.positionCS = TransformObjectToHClip(v.positionOS);
+                o.uv = v.uv;
+                return o;
+            }
+
+            float4 frag (Varyings i) : SV_Target
+            {
+                float x = abs(i.uv.x * 2 - 1);
+                float edge = pow(1 - x, _Softness);
+
+                float3 col = _Color.rgb * edge * _Intensity;
+                return float4(col, edge);
+            }
+
+            ENDHLSL
+        }
+    }
+}
