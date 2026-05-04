1779f0e482d314d194413f2e543027d4324afbb2
hoangtongvu <hoangtongvu@gmail.com>
2026-05-04 16:05:30 +0700

    Add HydrostaticPressureCalculator

---
+++ Assets/Scripts/Game.Mono/HydrostaticPressure/HydrostaticPressureCalculator.cs
@@ -0,0 +1,60 @@
+using Game.Domain.PubSub.Messengers;
+using Game.Domain.WorldFluid;
+using Game.ScriptableObjects;
+using Reflex.Attributes;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using ZBase.Foundation.PubSub;
+
+namespace Game.Mono.HydrostaticPressure
+{
+    public class HydrostaticPressureCalculator : MonoBehaviour
+    {
+        [Inject] private WaterSurface water;
+
+        private WaterSearchParameters Search;
+        private WaterSearchResult SearchResutt;
+
+        [Inject] private FluidsSO fluidsSO;
+        [Inject] private WorldFluidId worldFluidId;
+        private ISubscription fluidChangedMessageSub;
+
+        private const float atmospherePressure = 101325; // Pascal
+        private float fluidDensity; //kg/m^3
+        public float depth; // meter
+        public float pressure; // Pascal
+
+        private void Start()
+        {
+            SetFluidDensity(worldFluidId);
+        }
+
+        private void OnEnable()
+        {
+            fluidChangedMessageSub = GameplayMessenger.MessageSubscriber
+                .Subscribe<FluidKindChangedMessage>(message => SetFluidDensity(message.FluidId));
+        }
+
+        private void OnDisable()
+        {
+            fluidChangedMessageSub.Dispose();
+        }
+
+        private void SetFluidDensity(WorldFluidId id)
+        {
+            fluidDensity = fluidsSO.Value[id.Value].Density;
+        }
+
+        private void FixedUpdate()
+        {
+            Search.startPositionWS = transform.position;
+            water.ProjectPointOnWaterSurface(Search, out SearchResutt);
+
+            depth = SearchResutt.projectedPositionWS.y - transform.position.y;
+            if (depth < 0)
+                depth = 0;
+
+            pressure = atmospherePressure + fluidDensity * Mathf.Abs(Physics.gravity.y) * depth;
+        }
+    }
+}
